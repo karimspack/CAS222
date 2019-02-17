c5ff9d5243692d0fdce5a7e2e554f91f8c66b464
karimspack <46553203+karimspack@users.noreply.github.com>
2019-02-16 18:25:54 -0800

    Add files via upload

---
+++ template/index.php
@@ -0,0 +1,64 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Ace in the Hole MultiSport Weekend - Template -</title>
+    <meta name="description" content="Template">
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="css/reset.css" rel="stylesheet" type="text/css">
+    <link href="css/helper.css" rel="stylesheet" type="text/css">
+    <link href="css/style.css" rel="stylesheet" type="text/css">
+	<link href="css/grid.css" rel="stylesheet" type="text/css">
+    <link href='https://fonts.googleapis.com/css?family=Bangers' rel='stylesheet' type='text/css'>
+    <!--[if lt IE 9]>
+    <script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
+    <![endif]-->
+</head>
+<body>
+
+<?php include 'includes/header.inc.html.php'; ?>
+<?php include 'includes/nav.inc.html.php'; ?>
+
+        <main>
+            <aside id="exampleimg">Hero image goes here!</aside>
+            <h2>Heading 2 (h2)</h2>
+            <p> Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has
+				been the industry's standard dummy text ever since the 1500s, when an unknown printer took a
+				galley of type and scrambled it to make a type specimen book. It has survived not only five
+				centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was
+				popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages,
+				and more recently with desktop publishing software like Aldus PageMaker including
+				versions of Lorem Ipsum.
+            </p>
+            <h3>Heading 3 (h3)</h3>
+            <ul>
+                <li>List item (li)</li>
+                <li>List item (li)</li>
+                <li>List item (li)</li>
+                <li>List item (li)</li>
+            </ul>
+			<section id="images" class="section group">
+				<div class="col span_4_of_12">
+					<div class="images">1</div>
+					<h4>Image 1</h4>
+				</div>
+				<div class="col span_4_of_12">
+					<div class="images">2</div>
+					<h4>Image 2</h4>
+				</div>
+				<div class="col span_4_of_12">
+					<div class="images">3</div>
+					<h4>Image 3</h4>
+				</div>
+			</section>
+
+        </main>
+<?php include 'includes/footer.inc.html.php'; ?>
+
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+
+<script src="scripts/mobile.js"></script>
+
+</body>
+</html>
