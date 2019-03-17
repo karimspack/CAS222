562b4296abbcdc080176778cd76dcefc25793521
karimspack <46553203+karimspack@users.noreply.github.com>
2019-03-16 22:42:08 -0700

    Add files via upload

--- index.php
+++ index.php
@@ -1,26 +1,140 @@
-<?php
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Ace in the Hole MultiSport Weekend :: About</title>
+    <meta name="description" content="Ace in the Hole Multisport Events is proud to offer running and triathlon events to athletes of all shapes and sizes, national origins, sexual orientations and cultural backgrounds. We offer Events for Every Body.">
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="../css/reset.css" rel="stylesheet" type="text/css">
+    <link href="../css/helper.css" rel="stylesheet" type="text/css">
+    <link href="../css/style.css" rel="stylesheet" type="text/css">
+    <link href="../css/grid.css" rel="stylesheet" type="text/css">
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link href='https://fonts.googleapis.com/css?family=Bangers' rel='stylesheet' type='text/css'>
+    <!--[if lt IE 9]>
+    <script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
+    <![endif]-->
+</head>
+<body>
+		<?php include '../includes/header.inc.html.php'; ?>
+		<?php include '../includes/nav.inc.html.php'; ?>
 
-include 'includes/db.inc.html.php';
 
-try
-{
-    $sql = 'SELECT * FROM content WHERE id=1';
-    $result = $pdo->query($sql);
-}
-catch (PDOException $e)
-{
-    $error = 'Error fetching: ' . $e->getMessage();
-    include 'includes/error.html.php';
-    exit();
-}
 
-while ($row = $result->fetch())
-{
-    $contents[] = array(
-        'title' => $row['title'],
-        'description' => $row['description'],
-        'image' => $row['image'],
-    );
-}
+<main>
+		
+  			<img class="mb" src="../images/daytime-motion-ocean-1249545.jpg" alt="Image of swimers">
 
-include 'home/home.html.php';
+		<div class="slideshow-container">
+
+			<div class="mySlides fade">
+			  <div class="numbertext">1 / 3</div>
+			  <img src="../images/action-active-activity-1449054.jpg" style="width:100%">
+			  <!-- <div class="text">Example Text</div> -->
+			</div>
+
+			<div class="mySlides fade">
+			  <div class="numbertext">2 / 3</div>
+			  <img src="../images/action-active-athlete-1415810.jpg" style="width:100%">
+			  <!-- <div class="text">Example Two</div> -->
+			</div>
+
+			<div class="mySlides fade">
+			  <div class="numbertext">3 / 3</div>
+			  <img src="../images/adult-athlete-effort-1073094.jpg" style="width:100%">
+			  <!-- <div class="text">Example Three</div> -->
+			</div>
+
+			<a class="prev" onclick="plusSlides(-1)">&#10094;</a>
+			<a class="next" onclick="plusSlides(1)">&#10095;</a>
+
+		</div><!-- end of .slideshow-container-->
+			<br>
+			<div style="text-align:center">
+			  <span class="dot" onclick="currentSlide(1)"></span> 
+			  <span class="dot" onclick="currentSlide(2)"></span> 
+			  <span class="dot" onclick="currentSlide(3)"></span> 
+			</div><!--end of dots div-->
+	<h2>The Annual Ace in the Hole MultiSport Weekend </h2>
+	
+    <p> 
+		The Annual Ace in the Hole MultiSport Weekend is a legendary event in the Oregon triathlon and running community. It has become a traditional destination race for athletes from across the nation. 
+    </p>
+	<p>
+		There is something for every level of athletic ability. The weekend includes a first timer triathlon, a sprint, Olympic, and Half-Iron triathlons and 10K and Half marathon runs. Come to experience your first race or come to compete to win, but make sure you come to have fun!
+	</p>
+	<h3>What to Bring</h3>
+	<p>
+		Watch the weather closely.  The show goes on no matter what the weather is doing.<br>
+
+		You must wear your chip timing piece during the entire event.  You will be given a band that will hold your chip timing piece around your ankle throughout the entire event. Be sure that it is snapped tightly. Be sure to have your Chip Timing piece on before you start the race and be sure to step over the timing mats after each segment of the race. If you are wearing a wet-suit, make sure the timing piece goes under your wetsuit otherwise, you will have a very difficult time getting off your wet suit.
+
+	</p>
+	    <section class="section group">
+		<div class="col span_6_of_12">
+            <div>
+				<img src="../images/swim.png" alt="Icon for swiming" title="Icon for swiming">
+			</div>
+        </div>
+        <div class="col span_6_of_12">
+            <div>
+				<h3>Swim</h3>
+				<p id="baglicolor">Wetsuits are optional for the swim but will provide buoyancy and warmth. However, many people opt for no wetsuit for a triathlon so no worries.  We will provide you with a swim cap, but you will want to bring your own goggles.</p>
+			</div>
+        </div>
+		</section>
+		<section class="section group">
+        <div class="col span_6_of_12">
+            <div id="imageicon">
+				<img src="../images/bike.png" alt="Icon of a Bike" title="Icon of a Bike">
+			</div>
+        </div>
+	    <div class="col span_6_of_12">
+            <div>
+				<h3>Bike</h3>
+				<p id="bagcolor">A biking helmet is mandatory.  You will also receive 2 stickers in your package with your race number on them.  The small sticker should go on the front of your helmet.  The bigger sticker will wrap around your bike frame.  Road or mountain bikes are acceptable.  For safety reasons, be sure to have end-caps on the end of your handle-bars.</p>
+			</div>
+        </div>
+		</section>
+		<section class="section group">
+		<div class="col span_6_of_12">
+            <div>
+				<img src="../images/run.png" alt="Icon of a Running Activity" title="Icon of a Running Activity">
+			</div>
+        </div>
+	    <div class="col span_6_of_12">
+            <div>
+				<h3>Run</h3>
+				<p id="bagdcolor">You must finish the race with your bib number on the front of you.  Some people choose to pin it on at the beginning of the race and have it on for the whole event so they don’t have to worry about it.  Others pin it on a singlet that they put on once they finish the swim before they head out for the bike.  Others use an elastic racing strap that they pin their bib number to and then quickly strap it on before they leave for the run.  Use whatever option feels best for you.</p>
+			</div>
+        </div>
+    </section>
+	
+	<section id="diff">
+		<p>
+			Remember to bring a change of clothing so you can enjoy the post-event festivities. 
+		</p>
+	</section>
+	<h2>PACKET PICK UP</h2>
+	<p>
+		All packet pick up will occur at Why Worry Racing, 123 NW Everett, Portland OR.<br> Packet pick up hours: Thursday 8-5, Friday 9-noon.<br> No day of event packet pick up.
+	</p>
+	
+	<section>
+		<a class="weatherwidget-io" href="https://forecast7.com/en/45d52n122d68/portland/?unit=us" data-label_1="PORTLAND" data-label_2="WEATHER" data-icons="Climacons Animated" data-days="3" data-theme="dark" >PORTLAND WEATHER</a>
+		<script>
+		!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0];if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src='https://weatherwidget.io/js/widget.min.js';fjs.parentNode.insertBefore(js,fjs);}}(document,'script','weatherwidget-io-js');
+		</script>
+	</section>
+
+</main>
+		<?php include '../includes/footer.inc.html.php'; ?>
+
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+
+<script src="../scripts/mobile.js"></script>
+<script src="../scripts/slide.js"></script>
+
+</body>
+</html>
