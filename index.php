36e63c605c15b8a7d06688e9c02c6329d22020cb
karimspack <46553203+karimspack@users.noreply.github.com>
2019-03-16 23:56:37 -0700

    index.php for (FAQ)

--- index.php
+++ index.php
@@ -1,26 +1,84 @@
-<?php
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Ace in the Hole MultiSport Weekend :: About</title>
+    <meta name="description" content="Ace in the Hole Multisport Events is proud to offer running and triathlon events to athletes of all shapes and sizes, national origins, sexual orientations and cultural backgrounds. We offer Events for Every Body.">
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="../css/reset.css" rel="stylesheet" type="text/css">
+    <link href="../css/helper.css" rel="stylesheet" type="text/css">
+    <link href="../css/style.css" rel="stylesheet" type="text/css">
+    <link href="../css/grid.css" rel="stylesheet" type="text/css">
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link href='https://fonts.googleapis.com/css?family=Bangers' rel='stylesheet' type='text/css'>
+    <!--[if lt IE 9]>
+    <script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
+    <![endif]-->
+</head>
+<body>
+		<?php include '../includes/header.inc.html.php'; ?>
+		<?php include '../includes/nav.inc.html.php'; ?>
 
-include 'includes/db.inc.html.php';
 
-try
-{
-    $sql = 'SELECT * FROM content WHERE id=1';
-    $result = $pdo->query($sql);
-}
-catch (PDOException $e)
-{
-    $error = 'Error fetching: ' . $e->getMessage();
-    include 'includes/error.html.php';
-    exit();
-}
 
-while ($row = $result->fetch())
-{
-    $contents[] = array(
-        'title' => $row['title'],
-        'description' => $row['description'],
-        'image' => $row['image'],
-    );
-}
+<main>
+	<img class="mb" src="../images/daytime-motion-ocean-1249545.jpg" alt="Image of swimers">
 
-include 'home/home.html.php';
+    
+<dl>
+
+    <dt>What are the Rules?</dt>
+        <dd>
+        <p>We currently adhere to the USAT Rules for Triathlon &amp; Duathlon.  Important rules include no drafting, you must wear a helmet and music is ABSOLUTELY NOT allowed during the bike for obvious safety reasons.</p>
+        </dd>
+    
+    <dt>Can I use a personal music device while cycling? </dt>
+        <dd>
+        <p>Absolutely no music devices are allowed during the bike segment for obvious safety risks and will result in immediate disqualification.</p>
+        </dd>
+
+    <dt>Can I use a personal music device while running?</dt>
+        <dd>
+        <p>During any running segments, we prefer for athletes to NOT use music devices to maximize safety, assure a fair competitive environment and assure athletes can hear all course marshal instructions. With that said, we also understand that many athletes rely on music to help endure the challenge of running. To accommodate the needs of our athletes while still assuring maximum safety and a fair, competitive environment, here is the arrangement that we can accommodate.</p>
+			
+		<p>If you chose to use a personal music device, you must always keep one ear open for instructions so only one ear-bud can be used at any time. In addition, if you opt to use music, you will not be eligible for awards, points and rankings. So as an athlete, you have to make a choice…music or awards/points/rankings.</p>
+		
+		<p>If you are using a personal music devise with two earbuds, you will be immediately disqualified. Trust us, we don’t want to have to enforce a disqualification but in order to assure the safety of all athletes, this is a very important rule.</p>
+			
+		<p>We also request that if you chose to use a personal music device that you keep the volume to a minimum to assure all instructions can be heard.</p>
+       </dd>
+
+    <dt>Do I need to wear a wetsuit?</dt>
+        <dd>
+        <p>No, you do not need to wear a wetsuit. Many will not wear a wetsuit while others will choose to wear a wetsuit because of the buoyancy and warmth factor. </p>
+        </dd>
+
+    <dt>Do I have to use a road or racing bike?</dt>
+        <dd>
+        <p>No. We welcome any type of bike as long as it is functioning properly with brakes and endcaps at the end of your handlebars.</p>
+    </dd>
+
+</dl>
+
+	<section>
+		<a class="weatherwidget-io" href="https://forecast7.com/en/45d52n122d68/portland/?unit=us" data-label_1="PORTLAND" data-label_2="WEATHER" data-icons="Climacons Animated" data-days="3" data-theme="dark" >PORTLAND WEATHER</a>
+		<script>
+		!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0];if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src='https://weatherwidget.io/js/widget.min.js';fjs.parentNode.insertBefore(js,fjs);}}(document,'script','weatherwidget-io-js');
+		</script>
+	</section>
+</main>
+
+<?php include '../includes/footer.inc.html.php'; ?>
+
+
+<script src="../scripts/mobile.js"></script>
+<script src="../scripts/slide.js"></script>
+<script src="../scripts/faq.js"></scripts>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>    
+   
+
+
+</body>
+
+</html>
