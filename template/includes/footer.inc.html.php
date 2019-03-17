fd19c601fce7e3ea48b98430f48cbd40e3c00629
karimspack <46553203+karimspack@users.noreply.github.com>
2019-03-16 22:38:02 -0700

    Delete footer.inc.html.php

--- template/includes/footer.inc.html.php
+++
@@ -1,6 +0,0 @@
-<footer>
-    <aside id="exampleimg">Weather Widget goes here!</aside>
-    <div>
-        <p>Copyright &copy; Ace In The Hole MultiSport Weekend <?php echo date('Y'); ?> <br></p>
-    </div>
-</footer>
