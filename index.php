70571911713175cd17da185651ff9fb4e5b85a14
karimspack <46553203+karimspack@users.noreply.github.com>
2019-03-16 22:57:04 -0700

    Add files via upload

--- index.php
+++ index.php
@@ -1,26 +1,137 @@
-<?php
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Ace in the Hole MultiSport Weekend :: Courses</title>
+    <meta name="description" content="Ace in the Hole Multisport Events is proud to offer running and triathlon events to athletes of all shapes and sizes, national origins, sexual orientations and cultural backgrounds. We offer Events for Every Body.">
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="../css/reset.css" rel="stylesheet" type="text/css">
+    <link href="../css/helper.css" rel="stylesheet" type="text/css">
+    <link href="../css/style.css" rel="stylesheet" type="text/css">
+    <link href="../css/grid.css" rel="stylesheet" type="text/css">
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link href='https://fonts.googleapis.com/css?family=Bangers' rel='stylesheet' type='text/css'>
+    <!--[if lt IE 9]>
+    <script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
+    <![endif]-->
+</head>
+<body>
+<?php include '../includes/header.inc.html.php'; ?>
+<?php include '../includes/nav.inc.html.php'; ?>
 
-include 'includes/db.inc.html.php';
 
-try
-{
-    $sql = 'SELECT * FROM content WHERE id=1';
-    $result = $pdo->query($sql);
-}
-catch (PDOException $e)
-{
-    $error = 'Error fetching: ' . $e->getMessage();
-    include 'includes/error.html.php';
-    exit();
-}
 
-while ($row = $result->fetch())
-{
-    $contents[] = array(
-        'title' => $row['title'],
-        'description' => $row['description'],
-        'image' => $row['image'],
-    );
-}
+<main>
+    <img class="mb" src="../images/bicycles-bikes-sport-383.jpg" alt="Image of bikes">
 
-include 'home/home.html.php';
+    <div class="slideshow-container">
+
+        <div class="mySlides fade">
+            <div class="numbertext">1 / 3</div>
+            <img src="../images/action-active-activity-1449054.jpg" style="width:100%">
+            <!-- <div class="text">Example Text</div> -->
+        </div>
+
+        <div class="mySlides fade">
+            <div class="numbertext">2 / 3</div>
+            <img src="../images/action-active-athlete-1415810.jpg" style="width:100%">
+            <!-- <div class="text">Example Two</div> -->
+        </div>
+
+        <div class="mySlides fade">
+            <div class="numbertext">3 / 3</div>
+            <img src="../images/adult-athlete-effort-1073094.jpg" style="width:100%">
+            <!-- <div class="text">Example Three</div> -->
+        </div>
+
+        <a class="prev" onclick="plusSlides(-1)">&#10094;</a>
+        <a class="next" onclick="plusSlides(1)">&#10095;</a>
+
+    </div><!-- end of .slideshow-container-->
+    <br>
+    <div style="text-align:center">
+        <span class="dot" onclick="currentSlide(1)"></span>
+        <span class="dot" onclick="currentSlide(2)"></span>
+        <span class="dot" onclick="currentSlide(3)"></span>
+    </div><!--end of dots div-->
+    <h2>Upcoming Event: 23-24 March 2019</h2>
+    <p>
+        The Annual Ace in the Hole MultiSport Weekend is a legendary event in the Oregon triathlon and running community. It has become a traditional destination race for athletes from across the nation.
+    </p>
+    <h2>Course Details</h2>
+    <p>
+        WATER TEMPERATURE is expected to be between 62 – 66 degrees. The temperature will be taken on Friday and the morning of the race. Wetsuits are recommended.
+    </p>
+    <h1>Long Course</h1>
+    <h3>SWIM – 1.2mi </h3>
+    <p id="baglicolor">
+        Participants will make two counter-clockwise loops. Large buoys will mark the turn points. Kayakers will be positioned on the water to support the swimmers. Medical support will be present on the beach.
+    </p>
+    <h3>BIKE – 58 Miles </h3>
+    <p id="bagdcolor">
+        A scenic point-to-point course that travels over gently rolling hills prior to three hard climbs. The bike course will be marked with large directional signage and there will be course marshals at key intersections to help direct you.
+    </p>
+    <h3>RUN – 13.1mi </h3>
+    <p id="bagcolor">
+        A mostly flat loop course on widely paved bike paths that traverse through and around this beautiful and scenic destination resort (two hills total with a minimal elevation gain).
+    </p>
+    <h1>OLYMPIC Course</h1>
+    <h3>SWIM – 1,500 meters </h3>
+    <p id="baglicolor">
+        Participants will make two counter-clockwise loops. Large buoys will mark the turn points. Kayakers will be positioned on the water to support the swimmers. Medical support will be present on the beach.
+    </p>
+    <h3>BIKE – 28mi </h3>
+    <p id="bagdcolor">
+        A scenic point-to-point course that travels over gently rolling hills. The bike course will be marked with large directional signage and course marshals will be present at key intersections.
+    </p>
+    <h3>RUN – 10K </h3>
+    <p id="bagcolor">
+        A mostly flat loop course on widely paved bike paths that traverse through and around this beautiful and scenic destination resort (one hill total with a minimal elevation gain).
+    </p>
+    <h1>Sprint</h1>
+    <p id="baglicolor">
+        Course will offer a 1-loop 1/2 mile swim, the exact same 28 mile Bike Course as the Olympic distance and a 5km run.
+    </p>
+    <h1>Try-A-Tri</h1>
+    <p id="bagdcolor">
+        This novice race is designed for the first time triathlete, those new to the sport, our Junior Triathletes. The swim is a shorter, more manageable 1/4 mile distance, (instead of the standard 1/2-mile Sprint distance swim), 10 mile bike ride (vs 12 miles and it's a 2 loop course making it very spectator friendly!) and a flat 2 mile run (vs 3 mile sprint course).
+    </p>
+    <h1>Half Marathon</h1>
+    <p id="baglicolor">
+        Half-Marathon (13.1-miles): Half-Marathon event starts and finishes in the Athletes Village to the cheers of the enthusiastic crowd. Once finished, runners can enjoy the finish line festivities, including the Sports &amp; Fitness Expo and live entertainment. Post-race refreshments will be provided and the Awards Ceremony for the Half-Marathon will begin once the results have been certified
+    </p>
+    <h1>10k COURSE</h1>
+    <p id="bagcolor">
+        The 10K event starts and finishes in the Athletes Village. The paths are approximately 6 ft wide, perfectly paved and wind around through the forest. Each course has only two small hills with a minimal elevation gain to navigate and a fast downhill to flat finish to the roaring cheers of the crowd.
+    </p>
+    <h1>The Splash n' Dash</h1>
+    <p id="baglicolor">The Splash n' Dash is a kids event for kids ages 6 to 14. It is a 100 yard swim and a 50 meter dash. It is free if an adult registers or $25 if they do not.</p>
+
+    <section id="diff">
+        <p>
+            Remember to bring a change of clothing so you can enjoy the post-event festivities.
+        </p>
+    </section>
+    <h2>PACKET PICK UP</h2>
+    <p>
+        All packet pick up will occur at Why Worry Racing, 123 NW Everett, Portland OR.<br> Packet pick up hours: Thursday 8-5, Friday 9-noon.<br> No day of event packet pick up.
+    </p>
+
+    <section>
+        <a class="weatherwidget-io" href="https://forecast7.com/en/45d52n122d68/portland/?unit=us" data-label_1="PORTLAND" data-label_2="WEATHER" data-icons="Climacons Animated" data-days="3" data-theme="dark" >PORTLAND WEATHER</a>
+        <script>
+            !function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0];if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src='https://weatherwidget.io/js/widget.min.js';fjs.parentNode.insertBefore(js,fjs);}}(document,'script','weatherwidget-io-js');
+        </script>
+    </section>
+
+</main>
+<?php include '../includes/footer.inc.html.php'; ?>
+
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+
+<script src="../scripts/mobile.js"></script>
+<script src="../scripts/slide.js"></script>
+
+</body>
+</html>
